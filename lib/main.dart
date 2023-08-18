f1e7e8474ae5a8733484bb85c388b7a0f25e8c67
waleed-ahmed-cowlar <waleed.ahmed.cowlar@gmail.com>
2023-08-18 15:25:32 +0500

    App title renamed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter weather App',
       themeMode: ThemeMode.dark,
       theme: ThemeData(
         primarySwatch: Colors.blue,
